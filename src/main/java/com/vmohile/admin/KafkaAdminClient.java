42646dacdaa8ebd8b83f0fc9b29bd22c64618b41
high_in_entropy <iraj.mohile2013@gmail.com>
2024-07-06 10:41:32 +0000

    initial commit, setting things up

---
+++ src/main/java/com/vmohile/admin/KafkaAdminClient.java
@@ -0,0 +1,13 @@
+package com.vmohile.admin;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+
+import java.util.Properties;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+
+public class KafkaAdminClient {
+
+}
