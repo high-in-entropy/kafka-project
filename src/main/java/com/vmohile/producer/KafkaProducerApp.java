aeba6e8f24502741396b3bbd39b470d99a02e4dd
high_in_entropy <iraj.mohile2013@gmail.com>
2024-07-10 17:18:38 +0000

    A simple producer configured

--- src/main/java/com/vmohile/producer/KafkaProducerApp.java
+++ src/main/java/com/vmohile/producer/KafkaProducerApp.java
@@ -1,5 +1,28 @@
 package com.vmohile.producer;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Properties;
+
 public class KafkaProducerApp {
-    
+
+    public static void main (String[] args) {
+
+        String bootStrapServer = "10.0.0.127:9092";
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, String> record = new ProducerRecord<String,String>("first-topic", "This is my 2nd message!!");
+
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
 }
